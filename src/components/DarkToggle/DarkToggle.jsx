62f9cc83d04da0e8fa36ecbb9c8ede57da61713e
Aaron Stroud <astroud@gmail.com>
2021-05-19 10:55:51 -0700

    feat: Add sound effects to darkmode toggle

--- src/components/DarkToggle/DarkToggle.jsx
+++ src/components/DarkToggle/DarkToggle.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import useSound from 'use-sound'
 import StyledWrapper from './DarkToggle.elements'
 import { ReactComponent as DarkmodeSVG } from './darkmode.svg'
 import { ReactComponent as LightmodeSVG } from './lightmode.svg'
+import onSfx from './on.mp3'
+import offSfx from './off.mp3'
 
 const displaySVG = (theme) => {
   if (theme === 'dark') {
@@ -11,15 +14,25 @@ const displaySVG = (theme) => {
   return <DarkmodeSVG />
 }
 
-const DarkToggle = ({ themeToggler, theme }) => (
-  <StyledWrapper>
-    <label htmlFor="theme">
-      {displaySVG(theme)}
-      <input type="checkbox" id="theme" onChange={themeToggler} checked={theme === 'light'} />
-    </label>
-  </StyledWrapper>
-)
+const DarkToggle = ({ themeToggler, theme }) => {
+  const [playOnSfx] = useSound(onSfx)
+  const [playOffSfx] = useSound(offSfx)
 
+  return (
+    <StyledWrapper>
+      <label htmlFor="theme">
+        {displaySVG(theme)}
+        <input
+          type="checkbox"
+          id="theme"
+          onChange={themeToggler}
+          checked={theme === 'light'}
+          onClick={theme === 'light' ? playOffSfx : playOnSfx}
+        />
+      </label>
+    </StyledWrapper>
+  )
+}
 DarkToggle.propTypes = {
   themeToggler: PropTypes.func.isRequired,
   theme: PropTypes.string.isRequired,
---
+++ src/components/DarkToggle/README.md
@@ -0,0 +1,15 @@
+Audio Credits:
+
+## Deathscyp, October 8th, 2017
+
+Breaker switch flipped, edited on audacity. Cheers!
+
+[off.mp3](https://freesound.org/people/Deathscyp/sounds/404049/)
+[Creative Commons 0 License](https://creativecommons.org/publicdomain/zero/1.0/)
+
+## Joseph Sardin, BigSoundBank.com
+
+Switch desk lamp
+
+[on.mp3](https://bigsoundbank.com/detail-0036-switch-desk-lamp.html)
+[Creative Commons 0 License](https://creativecommons.org/publicdomain/zero/1.0/)
